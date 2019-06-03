e4041860b8ee40a630bbe15f4289ad980fef9726
calumabarnett <calum.barnett@digital.justice.gov.uk>
2019-06-03 13:46:31 +0100

    Update config

--- config.rb
+++ config.rb
@@ -1,3 +1,9 @@
 require 'govuk_tech_docs'
 
 GovukTechDocs.configure(self)
+
+set :relative_links, true
+
+set :css_dir, "/user-guidance/stylesheets"
+set :js_dir, "/user-guidance/javascripts"
+set :image_dir, "/user-guidance/images"
